e18cb95dbd68d5f565821340d55dcd31e4c5d7c6
Milyukov Sergey <serrguk@gmail.om>
2025-01-30 21:00:59 +0700

    Lesson 7, task 4 complete

---
+++ src/com/walking/lesson_7_4_RecursiveSqrt/Main.java
@@ -0,0 +1,28 @@
+package com.walking.lesson_7_4_RecursiveSqrt;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int input = scanner.nextInt();
+        scanner.close();
+        System.out.println(nestedRoots(input));
+    }
+
+    static double nestedRoots(int a) {
+        if (a < 0) {
+            System.out.println("Число должно быть положительным");
+            return 0;
+        }
+        return calculate(1, a);
+    }
+
+    static double calculate(int number, int maxNumber) {
+        if (number == maxNumber) {
+            return Math.sqrt(number);
+        }
+        return Math.sqrt(number + calculate(number + 1, maxNumber));
+    }
+}
