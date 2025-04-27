78dc991f72454698d5ec6521974e77b5d0ef3b44
Milyukov Sergey <serrguk@gmail.om>
2025-04-27 17:30:24 +0700

    lesson 13 started

---
+++ src/com/walking/lesson13_Inheritance/Animals/Animal.java
@@ -0,0 +1,7 @@
+package com.walking.lesson13_Inheritance.Animals;
+
+public class Animal {
+    protected Animal() {
+        System.out.println("I'm animal");
+    }
+}
---
+++ src/com/walking/lesson13_Inheritance/Animals/Main.java
@@ -0,0 +1,7 @@
+package com.walking.lesson13_Inheritance.Animals;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+    }
+}
