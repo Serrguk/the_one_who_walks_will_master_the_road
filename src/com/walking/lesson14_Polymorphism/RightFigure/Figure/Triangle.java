8f95d9190eee07c102c9fe2cff806ed4e2dc0da6
Milyukov Sergey <serrguk@gmail.om>
2025-05-04 00:07:47 +0700

    lesson 14 started, created all classes

---
+++ src/com/walking/lesson14_Polymorphism/RightFigure/Figure/Triangle.java
@@ -0,0 +1,6 @@
+package com.walking.lesson14_Polymorphism.RightFigure.Figure;
+
+import com.walking.lesson14_Polymorphism.RightFigure.RightFigure;
+
+public class Triangle extends RightFigure {
+}
---
+++ src/com/walking/lesson14_Polymorphism/RightFigure/RightFigure.java
@@ -0,0 +1,12 @@
+package com.walking.lesson14_Polymorphism.RightFigure;
+
+public class RightFigure {
+    public static final String DOWN_LINE = "_";
+    public static final String VERTICAL_LINE = "|";
+    public static final String RIGHT_LINE = "/";
+    public static final String LEFT_LINE = "\\";
+
+    public void draw(int side) {
+
+    }
+}
